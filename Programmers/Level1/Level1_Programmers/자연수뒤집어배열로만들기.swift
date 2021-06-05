099de96abcc2e72957cc4f5407a8b72bfa0f2c46
김설희 <2750seolhee@naver.com>
2021-06-05 17:36:39 +0900

    프로그래머스 레벨1 자릿수 더하기, 자연수 뒤집어 배열로 만들기 문제 풀이

---
+++ Programmers/Level1/Level1_Programmers/자연수뒤집어배열로만들기.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ n:Int64) -> [Int] {
+    return String(n).map{Int(String($0))!}.reversed()
+    // String으로 값을 캐스팅하고, map 함수 사용하여 전체 String 값을 Int로 바꿔준 후 reversed
+}
+
+print(solution(12345))
+
---
+++ Programmers/Level1/Level1_Programmers/자릿수더하기.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ n:Int) -> Int
+{
+    var answer:Int = 0
+    answer = String(n).map{Int(String($0))!}.reduce(0, +)
+    // String으로 캐스팅 한 후, String 값을 모두 Int로 캐스팅, 배열의 모든 값을 더해주었음
+
+    print(answer)
+    return answer
+}
+
+solution(123)
+solution(987)
