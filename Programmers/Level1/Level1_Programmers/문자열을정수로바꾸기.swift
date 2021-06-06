5cf96d368deac7702b60496532b391cfd7ae4912
김설희 <2750seolhee@naver.com>
2021-06-06 13:04:10 +0900

    Programmers Lv1 약수의 합, 문자열을 정수로 바꾸기 문제 풀이

---
+++ Programmers/Level1/Level1_Programmers/문자열을정수로바꾸기.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Level1_Programmers
+//
+//  Created by 설희pc on 2021/06/06.
+//
+
+import Foundation
+
+func solution(_ s:String) -> Int {
+    return Int(s)! // Int로 형변환, 강체추출 키워드 사용해주었음
+}
+
+print(solution("1234"))
+print(solution("-1234"))
---
+++ Programmers/Level1/Level1_Programmers/약수의합.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var sum = 0
+    
+    // 1부터 주어진 수 n까지 차례로 나눠보며 나머지가 0이 될 때 해당 수를 더해줌
+    for i in 1..<n+1 {
+        if n % i == 0 {
+            sum += i
+        }
+    }
+    
+    return sum
+}
+
+print(solution(12))
+print(solution(5))
+
+
+
