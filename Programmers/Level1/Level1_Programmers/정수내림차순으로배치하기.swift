7dcfbd4530b20e4796c22370cb660f5b80bc394b
김설희 <2750seolhee@naver.com>
2021-06-12 21:36:36 +0900

    정수 내림차순으로 배치하기 다시 풀어보기

--- Programmers/Level1/Level1_Programmers/정수내림차순으로배치하기.swift
+++ Programmers/Level1/Level1_Programmers/정수내림차순으로배치하기.swift
@@ -1,10 +1,14 @@
+// 프로그래머스 레벨1. 정수 내림차순으로 배치하기
+//
+// 두 바퀴 째 - 숫자를 문자열로 변환하고, 그걸 정렬하여 나온 배열을 joined 하려고 했는데 왜 안되는지ㅡㅜ,,
+// 아무리 해도 모르겠어서 첫 번째 풀이로 간다......
+
 import Foundation
 
 func solution(_ n:Int64) -> Int64 {
-    // sorted(by :) 사용, Int64로 입력된 n을 String으로 변환하여 정렬 후 다시 Int64로 변환
-    print(Int64(String(String(n).sorted(by: >))) ?? 0)
-    return Int64(String(String(n).sorted(by: >))) ?? 0
+    // sorted(by :) 사용, n을 String으로 변환, 그 값을 정렬하여 나온 배열을 다시 문자열로 변환한 후 숫자로 변환.
+    return Int64(String(String(n).sorted(by: >)))!
 }
 
 // 입출력 예시
-solution(118372)
+print(solution(118372))
