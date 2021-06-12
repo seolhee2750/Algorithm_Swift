6192026480633326a42ea842b09916954381563f
김설희 <2750seolhee@naver.com>
2021-06-13 00:45:48 +0900

    이상한 문자 만들기 문제 다시 풀어보기

---
+++ Programmers/Level1/Level1_Programmers/이상한문자만들기.swift
@@ -0,0 +1,50 @@
+// 프로그래머스 레벨1. 이상한 문자 만들기
+//
+// components 사용하여 풀이
+//
+// 두 바퀴 째 - 첫 번째 풀이보다 가독성도 좋고 로직도 이게 더 나은듯!!
+
+import Foundation
+
+// 첫 번째 풀이
+func solution1(_ s:String) -> String {
+    var arr = s.components(separatedBy: " ")
+
+    for i in 0..<arr.count {
+        var temp = ""
+        for (j, char) in arr[i].enumerated() {
+            if j % 2 == 0 {
+                temp += char.uppercased()
+            }
+            else {
+                temp += char.lowercased()
+            }
+        }
+        arr[i] = temp
+    }
+
+    return arr.joined(separator: " ")
+}
+
+// 두 바퀴 째 풀이
+func solution2(_ s:String) -> String {
+    var result = ""
+    var count = 0
+    
+    for i in s {
+        if i == " " {
+            result.append(" ")
+            count = 0
+        }
+        else {
+            count % 2 == 0 ? result.append(String(i.uppercased())) : result.append(String(i.lowercased()))
+            count += 1
+        }
+    }
+    return result
+}
+
+// 입출력 예시
+print(solution1("try hello world"))
+
+print(solution2("try hello world"))
