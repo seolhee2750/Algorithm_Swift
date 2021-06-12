312e91944e353f1bceb1e573d8611c00556579f8
김설희 <2750seolhee@naver.com>
2021-06-12 18:34:53 +0900

    짝수와 홀수 문제 다시 풀기

--- Programmers/Level1/Level1_Programmers/짝수와홀수.swift
+++ Programmers/Level1/Level1_Programmers/짝수와홀수.swift
@@ -1,11 +1,14 @@
+// 프로그래머스 레벨1. 짝수와 홀수
+//
+// 두 바퀴째 풀이 - 첫 번째 풀이랑 완전 똑같음
+
 import Foundation
 
 func solution(_ num:Int) -> String {
     // 2로 나눈 나머지가 0인지 아닌지에 따라 짝/홀 판별
-    print(num % 2 == 0 ? "Even" : "Odd")
     return num % 2 == 0 ? "Even" : "Odd"
 }
 
 // 입출력 예시
-solution(3)
-solution(4)
+print(solution(3))
+print(solution(4))
