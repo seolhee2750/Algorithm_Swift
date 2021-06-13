dbf58a4bb98a70421b1c44aebaa992cac9198021
김설희 <2750seolhee@naver.com>
2021-06-13 15:11:17 +0900

    문자열 내 p와 y의 개수 문제 다시 풀어보기

--- Programmers/Level1/Level1_Programmers/문자열내p와y의개수.swift
+++ Programmers/Level1/Level1_Programmers/문자열내p와y의개수.swift
@@ -1,6 +1,12 @@
+// 프로그래머스 레벨1. 문자열 내 p와 y의 개수
+//
+// 두 바퀴째 - 첫 번째 풀이가 시간복잡도가 훨씬 좋다.!!
+// 두 번째 풀이는 배열을 계속 검사해줘야해서 효율이 떨어지네ㅜ
+
 import Foundation
 
-func solution(_ s:String) -> Bool
+// 첫 번째 풀이
+func solution1(_ s:String) -> Bool
 {
     var countP = 0
     var countY = 0
@@ -15,6 +21,24 @@ func solution(_ s:String) -> Bool
     }
 
     return countP == countY
+}
+
+// 두 바퀴째 풀이
+func solution2(_ s:String) -> Bool
+{
+    var pCount = 0
+    var yCount = 0
+    
+    for i in s {
+        if Array("pP").contains(i) {
+            pCount += 1
+        }
+        else if Array("yY").contains(i) {
+            yCount += 1
+        }
+    }
+    
+    return pCount == yCount
 }
 
 // 입출력 예시
