54fab276e07c24a9908cfef31e5f7da7e88090ab
김설희 <2750seolhee@naver.com>
2021-06-13 13:35:44 +0900

    문자열을 정수로 바꾸기 다시 풀어보기

--- Programmers/Level1/Level1_Programmers/문자열을정수로바꾸기.swift
+++ Programmers/Level1/Level1_Programmers/문자열을정수로바꾸기.swift
@@ -1,7 +1,11 @@
+// 프로그래머스 레벨1. 문자열을 정수로 바꾸기
+//
+// 두 바퀴 째 - 첫 번재 풀이와 같음. -같이 부호 모양의 스트링도 정수로 인식할 수 있음,,
+
 import Foundation
 
 func solution(_ s:String) -> Int {
-    return Int(s)! // Int로 형변환, 강체추출 키워드 사용해주었음
+    return Int(s)! // Int로 형변환, 강체추출
 }
 
 // 입출력 예시
