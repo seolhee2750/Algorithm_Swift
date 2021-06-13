6bbf49ccb9c07627486a0237fb5be0cf0a8110d5
김설희 <2750seolhee@naver.com>
2021-06-13 15:59:24 +0900

    나누어 떨어지는 숫자 배열 다시 풀어보기

--- Programmers/Level1/Level1_Programmers/나누어떨어지는숫자배열.swift
+++ Programmers/Level1/Level1_Programmers/나누어떨어지는숫자배열.swift
@@ -1,6 +1,11 @@
+// 프로그래머스 레벨1. 나누어 떨어지는 숫자 배열
+//
+// 두 바퀴째 - 첫 번째 풀이와 거의 같지만 디테일한 부분에서 더 깔끔해졌음!
+
 import Foundation
 
-func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
+// 첫 번째 풀이
+func solution1(_ arr:[Int], _ divisor:Int) -> [Int] {
     var result = [Int]()
     
     // 배열 원소들을 하나씩 나눠보며 나눠 떨어지는 수들을 result 배열에 추가
@@ -14,7 +19,20 @@ func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
     return result.isEmpty ? [-1] : result.sorted(by : <)
 }
 
+// 두 바퀴째 풀이
+func solution2(_ arr:[Int], _ divisor:Int) -> [Int] {
+    var result = [Int]()
+    for i in 0..<arr.count {
+        if arr[i] % divisor == 0 { result.append(arr[i]) }
+    }
+    return result.isEmpty ? [-1] : result.sorted()
+}
+
 // 입출력 예시
-print(solution([5, 9, 7, 10], 5))
-print(solution([2, 36, 1, 3], 1))
-print(solution([3, 2, 6], 10))
+print(solution1([5, 9, 7, 10], 5))
+print(solution1([2, 36, 1, 3], 1))
+print(solution1([3, 2, 6], 10))
+
+print(solution2([5, 9, 7, 10], 5))
+print(solution2([2, 36, 1, 3], 1))
+print(solution2([3, 2, 6], 10))
