0c5a1b40b9315818343a00418583a8d2766d86e8
김설희 <2750seolhee@naver.com>
2021-06-13 16:34:23 +0900

    2016 문제 다시 풀어보기

--- Programmers/Level1/Level1_Programmers/2016년.swift
+++ Programmers/Level1/Level1_Programmers/2016년.swift
@@ -1,6 +1,11 @@
+// 프로그래머스 레벨1. 2016
+//
+// 두 바퀴째 - 첫 번째 풀이와 로직은 거의 같은데
+// day 배열을 첫 번째 풀이처럼 그냥 FRI부터 시작하게 했으면 조금 더 효율적이었을 듯
+
 import Foundation
 
-func solution(_ a:Int, _ b:Int) -> String {
+func solution1(_ a:Int, _ b:Int) -> String {
     // 1월 1일이 금요일이므로, 금요일부터 배열 생성
     let days = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
     // 모든 달의 일 수를 배열로 생성
@@ -15,4 +20,21 @@ func solution(_ a:Int, _ b:Int) -> String {
     return days[(dayCount + b - 1) % 7]
 }
 
-print(solution(5, 24))
+func solution2(_ a:Int, _ b:Int) -> String {
+    var monthDay = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    var day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+    var dayCount = 0
+    
+    for i in 0..<a-1 {
+        dayCount += monthDay[i]
+    }
+    
+    return day[(dayCount + 5 + b - 1) % 7]
+}
+
+// 입출력 예시
+print(solution1(5, 24))
+
+print(solution2(5, 24))
+
+
