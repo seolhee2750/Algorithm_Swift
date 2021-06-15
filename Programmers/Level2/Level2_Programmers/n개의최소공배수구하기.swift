8e88a7379b69c9d3f07441be1c88beceaaa9a282
김설희 <2750seolhee@naver.com>
2021-06-15 14:32:49 +0900

    n개의 최소공배수 구하기 문제 다시 풀어보기

--- Programmers/Level2/Level2_Programmers/n개의최소공배수구하기.swift
+++ Programmers/Level2/Level2_Programmers/n개의최소공배수구하기.swift
@@ -1,6 +1,11 @@
+// 프로그래머스 레벨2. n개의 최소공배수 구하기
+//
+// 두 바퀴째 - 훨씬 간결하고 효율적으로 작성한듯
+
 import Foundation
 
-func solution(_ arr:[Int]) -> Int {
+// 첫 번째 풀이
+func solution1(_ arr:[Int]) -> Int {
     return arr.reduce(1){LCM($0, $1)}
     // 0이랑 최소공배수 구해버리면 안되니까 1로 초기값 정해주고 LCM함수 돌려버리기
 }
@@ -31,9 +36,30 @@ func LCM(_ n:Int, _ m:Int) -> Int {
     return n*m/gcd
 }
 
+// 두 바퀴째 풀이
+func solution2(_ arr:[Int]) -> Int {
+    func lcmSolution(_ n1:Int, _ n2:Int) -> Int {
+        var gcd = n1
+        while gcd > 0 {
+            if (n1 % gcd == 0 && n2 % gcd == 0) {
+                break
+            }
+            else {
+                gcd -= 1
+            }
+        }
+        return n1 * n2 / gcd
+    }
+    
+    return arr.sorted().reduce(1){ lcmSolution($0, $1) }
+}
+
 // 입출력 예시
-print(solution([2, 6, 8, 14]))
-print(solution([1, 2, 3]))
+print(solution1([2, 6, 8, 14]))
+print(solution1([1, 2, 3]))
+
+print(solution2([2, 6, 8, 14]))
+print(solution2([1, 2, 3]))
 
 
 
