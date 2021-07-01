6d130bff4d7b76d5a8027e59664224a1b75593a0
김설희 <2750seolhee@naver.com>
2021-07-01 16:48:37 +0900

    순열 기본 로직 정리

--- AlgorithmNote/Permutation/Permutation/main.swift
+++ AlgorithmNote/Permutation/Permutation/main.swift
@@ -1,26 +1,2 @@
 import Foundation
 
-func permuDFS(_ phase: Int) {
-    if phase == r {
-        print(result)
-        return
-    }
-    else {
-        for i in (0..<numbers.count) {
-            if checkList[i] == false {
-                checkList[i] = true
-                result[phase] = numbers[i]
-                permuDFS(phase + 1)
-                checkList[i] = false
-            }
-        }
-    }
-}
-
-let numbers = [1, 2, 3]
-let r = 2
-
-var result = [Int](repeating: 0, count: r)
-var checkList = [Bool](repeating: false, count: numbers.count)
-
-permuDFS(0)
