0620b586648fbe23ac6b3ff54dfabc890c70ada9
김설희 <2750seolhee@naver.com>
2021-07-01 22:07:29 +0900

    순열 로직 재구성

--- AlgorithmNote/Permutation/Permutation/main.swift
+++ AlgorithmNote/Permutation/Permutation/main.swift
@@ -1,2 +1,38 @@
+// 순열 기본 로직 - 재구성
+//
+// 개수에 상관 없이, 주어진 숫자의 모든 조합을 구하는 함수를 구현해보자!@@
+
 import Foundation
+
+let numbers = [1, 2, 3, 4]
+var result = [Int]()
+var checkList = Array(repeating: false, count: numbers.count)
+var answer = [String]()
+var r = 0
+
+for i in 1...numbers.count {
+    r = i
+    result = Array(repeating: 0, count: r)
+    PermuSolution(0)
+}
+
+func PermuSolution(_ stage: Int){
+    if stage == r {
+        answer.append(result.map{String($0)}.joined())
+        return
+    }
+    else {
+        for i in 0..<numbers.count {
+            if checkList[i] == false {
+                checkList[i] = true
+                result[stage] = numbers[i]
+                PermuSolution(stage + 1)
+                checkList[i] = false
+            }
+        }
+    }
+}
+
+print(answer)
+
 
--- AlgorithmNote/Permutation/Permutation/순열기본로직.swift
+++ AlgorithmNote/Permutation/Permutation/순열기본로직.swift
@@ -1,3 +1,5 @@
+// 순열 기본 로직
+
 import Foundation
 
 func permuDFS(_ phase: Int) {
@@ -17,10 +19,10 @@ func permuDFS(_ phase: Int) {
     }
 }
 
-let numbers = [1, 2, 3]
-let r = 2
+let numbers = [1, 2, 3] // 입력 예시
+let r = 2 // 몇 개의 숫자를 뽑아 조합을 만들건지 !!
 
-var result = [Int](repeating: 0, count: r)
-var checkList = [Bool](repeating: false, count: numbers.count)
+var result = [Int](repeating: 0, count: r) // 조합된 숫자를 담을 배열
+var checkList = [Bool](repeating: false, count: numbers.count) // 조합을 위한 체크리스트
 
-permuDFS(0)
+permuDFS(0) // 실행문
