f9d50426a007a3b35d8d2bcf597fafe2e02d5519
김설희 <2750seolhee@naver.com>
2021-07-01 15:45:18 +0900

    소수 찾기 문제 풀이 수정

--- Programmers/Level2/Level2_Programmers/main.swift
+++ Programmers/Level2/Level2_Programmers/main.swift
@@ -3,6 +3,7 @@
 import Foundation
 
 func solution(_ numbers:String) -> Int {
+    let numArr = Array(numbers).map({String($0)})
     var r = 1
     var result = [String](repeating: "", count: numbers.count)
     var checkList = [Bool](repeating: false, count: numbers.count)
@@ -14,10 +15,10 @@ func solution(_ numbers:String) -> Int {
             else { everyNum.append(result.map({String($0)}).joined()); return }
         }
         else {
-            for i in (0..<numbers.count) {
+            for i in (0..<numArr.count) {
                 if checkList[i] == false {
                     checkList[i] = true
-                    result[phase] = numbers[i]
+                    result[phase] = numArr[i]
                     permuDFS(phase + 1)
                     checkList[i] = false
                 }
@@ -25,7 +26,7 @@ func solution(_ numbers:String) -> Int {
         }
     }
     
-    for _ in 0..<numbers.count {
+    for _ in 0..<numArr.count {
         permuDFS(0)
         r += 1
     }
@@ -34,17 +35,26 @@ func solution(_ numbers:String) -> Int {
     var count = 0
     
     for i in 0..<everyNum.count {
-        for j in 2..<Int(everyNum[i])! {
-            if i % j == 0 {
-                isNotPrime = true
-                break
-            }
+        if Int(everyNum[i])! < 4 {
+            if everyNum[i] == "2" { count += 1 }
+            if everyNum[i] == "3" { count += 1 }
         }
-        if isNotPrime {
-            count += 1
-            isNotPrime = false
+        else {
+            for j in 2..<Int(everyNum[i])! {
+                if i % j == 0 {
+                    isNotPrime = true
+                    break
+                }
+            }
+            if isNotPrime {
+                count += 1
+                isNotPrime = false
+            }
         }
     }
     
     return count
 }
+
+print(solution("17"))
+print(solution("011"))
