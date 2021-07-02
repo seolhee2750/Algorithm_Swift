3ea0a2329fa814192dca7f4630545fdf4894bbcf
김설희 <2750seolhee@naver.com>
2021-07-02 22:25:21 +0900

    올바른 괄호 문제 다시 풀어보기

--- Programmers/Level2/Level2_Programmers/올바른괄호.swift
+++ Programmers/Level2/Level2_Programmers/올바른괄호.swift
@@ -1,8 +1,11 @@
 // 프로그래머스 레벨2. 올바른 괄호
+//
+// 두 바퀴째 - 어려움 없이 풀 수 있었다. 글구 더 깔끔해짐 
 
 import Foundation
 
-func solution(_ s:String) -> Bool
+// 첫 번째 풀이
+func solution1(_ s:String) -> Bool
 {
     var sum = 0
     
@@ -23,8 +26,26 @@ func solution(_ s:String) -> Bool
     return sum == 0 ? true : false
 }
 
+// 두 바퀴째 풀이
+func solution2(_ s:String) -> Bool {
+    var sum = 0
+    
+    for i in s {
+        if i == "(" { sum += 1 }
+        else { sum -= 1 }
+        if sum < 0 { return false }
+    }
+
+    return sum != 0 ? false : true
+}
+
 // 입출력 예시
-print(solution("()()"))
-print(solution("(())()"))
-print(solution(")()("))
-print(solution("(()("))
+print(solution1("()()"))
+print(solution1("(())()"))
+print(solution1(")()("))
+print(solution1("(()("))
+
+print(solution2("()()"))
+print(solution2("(())()"))
+print(solution2(")()("))
+print(solution2("(()("))
