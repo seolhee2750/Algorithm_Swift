4dea49c2636547c8b29809c6b4737f63213d4048
김설희 <2750seolhee@naver.com>
2021-07-02 14:21:53 +0900

    순열,, 소수찾기,, 무한 수정 ㅜㅜ

--- Programmers/Level2/Level2_Programmers/main.swift
+++ Programmers/Level2/Level2_Programmers/main.swift
@@ -6,6 +6,7 @@ func solution(_ numbers: String) -> Int {
     var checkList = Array(repeating: false, count: numbers.count)
     var answer = [String]()
     var r = 0
+    var toCheckPrime = [Int]()
 
     func PermuSolution(_ stage: Int) {
         if stage == r {
@@ -24,46 +25,41 @@ func solution(_ numbers: String) -> Int {
         }
     }
     
-    var count = 0
-    let toCheckPrime = answer.map({Int($0)!})
-    
-    func PrimeCheck() {
+    func PrimeCheck(_ count: Int) -> Int {
+        var counting = count
         var isNotPrime = false
         
         for i in 0..<toCheckPrime.count {
             if toCheckPrime[i] < 4 {
                 if toCheckPrime[i] == 2 || toCheckPrime[i] == 3 {
-                    count += 1
+                    counting += 1
                 }
             }
             else  {
-                for j in 2...toCheckPrime[i] {
-                    if toCheckPrime[i] % j == 0 { isNotPrime = true; break }
+                for j in 2...Int(sqrt(Double(toCheckPrime[i]))) {
+                    if toCheckPrime[i] % j == 0 {
+                        isNotPrime = true
+                        break
+                    }
                 }
-                if isNotPrime == false { count += 1 }
+                if isNotPrime == false {
+                    counting += 1
+                }
             }
         }
         
-        return
+        return counting
     }
-    
-    var permuIsOver = false
     
     for i in 1...numbers.count {
         r = i
         result = Array(repeating: 0, count: r)
         PermuSolution(0)
-        if i == numbers.count { permuIsOver = true }
+        if i == numbers.count { toCheckPrime = answer.map({Int($0)!}) }
     }
     
-    if permuIsOver == true { PrimeCheck() }
-    
-    return count
+    return PrimeCheck(0)
 }
 
-print(solution("17"))
+// print(solution("17"))
 print(solution("011"))
-
-// 지금 문제가..
-// 함수 안에서 위에 함수 돌고 그담에 아래 함수 돌고 또 다시 위에 함수 돌고 이딴식임. 왜이러는지 모르겠는데
-// 그걸 고쳐야함..ㅜㅜ 디버깅하면서 원인을 찾아보자....
