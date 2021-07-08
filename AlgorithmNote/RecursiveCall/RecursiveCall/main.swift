78528b2e78905693ee0a3be7d76bdeac6b351970
김설희 <2750seolhee@naver.com>
2021-07-08 23:58:26 +0900

    피보나치 일반 재귀, 꼬리 재귀 비교

--- AlgorithmNote/RecursiveCall/RecursiveCall/main.swift
+++ AlgorithmNote/RecursiveCall/RecursiveCall/main.swift
@@ -2,10 +2,18 @@
 
 import Foundation
 
-func Fibo(_ n:Int) -> Int {
+// 꼬리 재귀
+func tailFibo(_ n: Int, _ result: Int) -> Int {
     if n == 0 { return 0 }
     else if n == 1 { return 1 }
-    return (Fibo(n - 1) + Fibo(n - 2)) % 1234567
+    return (tailFibo(n - 1) + tailFibo(n - 2))
+
+// 일반 재귀
+func Fibo(_ n: Int) -> Int {
+    if n == 0 { return 0 }
+    else if n == 1 { return 1 }
+    return (Fibo(n - 1) + Fibo(n - 2))
 }
 
 print(Fibo(3))
+print(tailFibo(3))
