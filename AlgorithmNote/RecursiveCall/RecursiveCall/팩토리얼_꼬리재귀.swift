c595fd6ccd5dc37fbc525465e04f493809d95f5d
김설희 <2750seolhee@naver.com>
2021-07-08 23:44:17 +0900

    팩토리얼 꼬리재귀 구현

---
+++ AlgorithmNote/RecursiveCall/RecursiveCall/팩토리얼_꼬리재귀.swift
@@ -0,0 +1,21 @@
+// 꼬리 재귀로 구현한 팩토리얼, 일반 재귀 비교
+
+import Foundation
+
+func tailRecursion(_ x: Int, _ fac: Int) -> Int {
+    if x == 1 {
+        return fac
+    }
+    return tailRecursion(x-1, x * fac)
+}
+
+// 일반적인 재귀
+func Recursion(_ x: Int) -> Int {
+    if x == 1 {
+        return 1
+    }
+    return x * Recursion(x-1)
+}
+
+print(tailRecursion(3, 1))
+print(Recursion(3))
