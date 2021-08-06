a459446ae4023b9c8ac71ac0cc3d99e87dc51c78
김설희 <2750seolhee@naver.com>
2021-08-06 18:15:34 +0900

    lv.2 다리를 지나는 트럭 문제 풀이 수정

--- Programmers/Level2/Level2_Programmers/다리를지나는트럭.swift
+++ Programmers/Level2/Level2_Programmers/다리를지나는트럭.swift
@@ -1,35 +1,30 @@
-// 프로그래머스 레벨2. 다리를 지나는 트럭
-
 import Foundation
 
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
-    var bridge = Array(repeating: 0, count: bridge_length)
-    var count = 0
-    var result = [Int]()
+    var stack = Array(repeating: 0, count: bridge_length)
     var truck = truck_weights
-     
-    while result.count != truck_weights.count {
-        if bridge[bridge.count-1] != 0 {
-            result.append(bridge[bridge.count-1])
-            bridge[bridge.count-1] = 0
-        }
-        for i in (0..<bridge.count-1).reversed() {
-            bridge[i+1] = bridge[i]
-            if i == 0 { bridge[i] = 0 }
-        }
-    
-        if truck.count > 0 && truck[0] + bridge.reduce(0, +) <= weight {
-            bridge[0] = truck[0]
-            truck.removeFirst()
-        }
-        
-        count += 1
-    }
+    var sec = 0
+    var now_weight = 0
 
-    return count
+    // 반복마다 트럭의 배열을 삭제
+    while !stack.isEmpty {
+        sec += 1
+        now_weight -= stack.removeFirst()
+
+        // 옵셔널 바인딩. 다리에 올라갈 트럭이 있을 때만 계산
+        if let t = truck.first {
+            if t + now_weight > weight {
+                stack.append(0)
+            }
+            else {
+                stack.append(t)
+                now_weight += truck.removeFirst()
+            }
+        }
+    }
+    return sec
 }
 
-// 입출력 예시
 print(solution(2, 10, [7, 4, 5, 6]))
 print(solution(100, 100, [10]))
-print(solution(100, 100, [10,10,10,10,10,10,10,10,10,10]))
+print(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))
