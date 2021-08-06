70a424ac0e6a85d0a53e30d124407398a7d76f90
김설희 <2750seolhee@naver.com>
2021-08-06 18:14:24 +0900

    lv.2 다리를 지나는 트럭 문제 풀이

---
+++ Programmers/Level2/Level2_Programmers/main.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
+    var stack = Array(repeating: 0, count: bridge_length)
+    var truck = truck_weights
+    var sec = 0
+    var now_weight = 0
+
+    // 반복마다 트럭의 배열을 삭제
+    while !stack.isEmpty {
+        sec += 1
+        now_weight -= stack.removeFirst()
+
+        // 옵셔널 바인딩. 다리에 올라갈 트럭이 있을 때만 계산
+        if let t = truck.first {
+            if t + now_weight > weight {
+                stack.append(0)
+            }
+            else {
+                stack.append(t)
+                now_weight += truck.removeFirst()
+            }
+        }
+    }
+    return sec
+}
+
+print(solution(2, 10, [7, 4, 5, 6]))
+print(solution(100, 100, [10]))
+print(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))
