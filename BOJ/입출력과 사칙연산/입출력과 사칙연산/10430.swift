4bf174a790e8fd07aabf532fda1bbdaee7fcc825
김설희 <2750seolhee@naver.com>
2021-09-03 18:08:03 +0900

    BOJ #10430 나머지 문제 풀이

---
+++ BOJ/입출력과 사칙연산/입출력과 사칙연산/10430.swift
@@ -0,0 +1,7 @@
+// BOJ #10430
+
+let num = readLine()!.split(separator: " ").map({Int(String($0))!})
+print((num[0]+num[1])%num[2])
+print(((num[0]%num[2]+(num[1]%num[2])))%num[2])
+print((num[0]*num[1])%num[2])
+print(((num[0]%num[2]*(num[1]%num[2])))%num[2])
--- BOJ/입출력과 사칙연산/입출력과 사칙연산/main.swift
+++
@@ -1,3 +0,0 @@
-// BOJ #10430
-
-import Foundation
